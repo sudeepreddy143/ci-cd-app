626285271f9fdd901d607b37d28080a9b706aa5e
sudeepreddy143 <Sudeeppanyam@gmail.com>
2025-03-31 06:59:11 +0530

    updated app folder

---
+++ app/main.py
@@ -0,0 +1,9 @@
+def hello_world():
+    return "Hello, Docker and GitHub Actions!"
+
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print(hello_world())
+    print(f"2 + 3 = {add(2, 3)}")
